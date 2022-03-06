071098530ea566a22c8d41334e944f2fc37cd7c1
root <mtrhoades0727@gmail.com>
2022-03-06 09:45:33 -1000

    multiple routes, using query params, using .env

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+// Home Page
+app.get('/', (req, res) => {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black;
+            height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+                <h1>White Page</h1>
+        </body>`);
+    console.log('Server is running!')
+});
+
+// Teal Page ... Now using query params:
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color;
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
+        </body>
+    `) // ^ adds uppercasing to first letter of h1.
+});
+
+// // Crimson Page
+// app.get('/crimson', function (req, res) {
+//     res.send(`
+//         <body style="margin: 0;">
+//             <div style="border: 1px solid black; height: 10vh; background-color: crimson;">
+//                 <h2 style="text-align: center;">NAV BAR</h2>
+//             </div>
+//             <h1 style="color: crimson;">Crimson Page</h1>
+//         </body>
+//     `)
+// });
+
+
+app.listen(process.env.PORT);
